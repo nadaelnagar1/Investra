f7b301bd211effdf4c76dc7ad70978cd11ab8303
nadaibrahim <nada.ibrahim.de@gmail.com>
2025-01-18 21:25:35 +0200

    - Created the Base Repository and it's interface. - implemented the base methods.

--- Investra_DAL/Repositories/GenericRepository/BaseRepository.cs
+++ Investra_DAL/Repositories/GenericRepository/BaseRepository.cs
@@ -1,6 +1,53 @@
-﻿namespace Investra_DAL.Repositories.GenericRepository
+﻿using Investra_DAL.Database.ApplicationDbContext;
+
+namespace Investra_DAL.Repositories.GenericRepository
 {
-    public class BaseRepository
+    public class BaseRepository<TEntity> : IBaseRepository<TEntity> where TEntity : class
     {
+        private readonly ApplicationDbContext _context;
+        public BaseRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<TEntity> AddAsync(TEntity entity)
+        {
+            await _context.Set<TEntity>().AddAsync(entity);
+            await SaveAsync();
+            return entity;
+        }
+
+        public async Task<TEntity> Delete(Guid id)
+        {
+            var entity = await _context.Set<TEntity>().FindAsync(id);
+            if (entity != null)
+            {
+                _context.Set<TEntity>().Remove(entity);
+                await SaveAsync();
+                return entity;
+            }
+            return null;
+        }
+
+        public async Task<IEnumerable<TEntity>> GetAllAsync()
+        {
+            return await _context.Set<TEntity>().ToListAsync();
+        }
+
+        public async Task<TEntity> GetByIdAsync(Guid id)
+        {
+            return await _context.Set<TEntity>().FindAsync(id);
+        }
+
+        public async Task SaveAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<TEntity> Update(TEntity entity)
+        {
+            _context.Entry(entity).State = EntityState.Modified; 
+            await SaveAsync();
+            return entity;
+        }
     }
 }
--- Investra_DAL/Repositories/GenericRepository/IBaseRepository.cs
+++ Investra_DAL/Repositories/GenericRepository/IBaseRepository.cs
@@ -1,6 +1,12 @@
 ﻿namespace Investra_DAL.Repositories.GenericRepository
 {
-    public interface IBaseRepository
+    public interface IBaseRepository<TEntity> where TEntity : class
     {
+        Task<TEntity> GetByIdAsync(Guid id);
+        Task<IEnumerable<TEntity>> GetAllAsync();
+        Task<TEntity> AddAsync(TEntity entity);
+        Task<TEntity> Delete(Guid id);
+        Task<TEntity> Update(TEntity entity);
+        Task SaveAsync();
     }
 }
